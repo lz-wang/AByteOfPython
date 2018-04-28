6d605b752c5e6fb7885e8f493c903f7bdc40aba5
lzwang <zzSWG@users.noreply.github.com>
2018-04-28 09:02:36 +0800

    错误更正
    line15 wa --> was

--- 09-Exceptions/exceptions_raise.py
+++ 09-Exceptions/exceptions_raise.py
@@ -12,6 +12,6 @@ try:
 except EOFError:
 	print('Why did you do an EOF on me?')
 except ShortInputException as ex:
-	print(('ShortInputException: The input wa ' + '{0} long, expected at least {1}').format(ex.length, ex.atleast))
+	print(('ShortInputException: The input was ' + '{0} long, expected at least {1}').format(ex.length, ex.atleast))
 else:
 	print('No exception was raised.')
